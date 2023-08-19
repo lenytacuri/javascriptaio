9df94eac69207cb419d02bb01eb6ef7be77a6486
Leny <tacurileny@gmail.com>
2023-08-19 17:47:51 -0500

    Second commit

--- gamelet.js
+++ gamelet.js
@@ -1,18 +1,6 @@
-const ball = document.getElementById("ball");
-document.addEventListener("keydown", handleKeyPress);
-let position = 0;
-function handleKeyPress(e) {
-  if (e.code === "ArrowLeft") {
-    position = position - 10;
-  }
-  if (e.code === "ArrowRight") {
-    position = position + 10;
-  }
-  if (position < 0) {
-    position = 0;
-  }
-  refresh();
-}
-function refresh() {
-  ball.style.left = position + "px";
-}
+const dt = Date();
+const hours = dt.getHours();
+let msg;
+msg = hours < 12 ? "Good morning!" : "Welcome";
+const el = document.getElementById("message");
+el.innerHTML = msg;
--- practice.js
+++
@@ -1,6 +0,0 @@
-const x = 5;
-function sum(a, b) {
-  return a + b;
-}
-const result = sum(x, 10);
-console.log(result);
